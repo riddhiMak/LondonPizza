adce6ef66ade546d49497d14ab090c335e6b0e8e
Riddhi <Riddhimakvana30@gmail.com>
2023-08-21 17:11:51 +0530

    Initialise app

---
+++ LondonPizza/LondonPizza/LondonPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LondonPizzaApp.swift
+//  LondonPizza
+//
+//  Created by Riddhi Makwana on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LondonPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
